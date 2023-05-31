b42ce10f660152adc85d299f0157047648d35a61
Marco Lehne <lehne.mar@gmail.com>
2023-05-31 18:41:14 +0200

    feat: set max amount of prompt and answers in ul

--- public/js/updateChatWindow.js
+++ public/js/updateChatWindow.js
@@ -3,6 +3,10 @@ const chatWindowId = 'chat-window-ul';
 export function updateChatWindow(text) {
 
     const chatWindow = document.getElementById(chatWindowId);
+    if (chatWindow.childElementCount >= 20) {
+        chatWindow.removeChild(chatWindow.firstElementChild);
+        chatWindow.removeChild(chatWindow.firstElementChild);
+      }
     const newListItem = document.createElement('li');
     newListItem.textContent = text;
     chatWindow.appendChild(newListItem);
